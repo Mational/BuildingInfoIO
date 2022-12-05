b2ccfa432de818b22357028f538098194647cb22
Mational <grzegorzmal40@gmail.com>
2022-12-05 12:37:18 +0100

    Poprawa szkieletu projektu
    Poprawiłem szkielet projektu.  Teraz powinien działać.

--- src/main/java/pl/put/poznan/buildinginfo/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/buildinginfo/app/TextTransformerApplication.java
@@ -1,10 +1,9 @@
-package pl.put.poznan.transformer.app;
+package pl.put.poznan.buildinginfo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.buildinginfo.rest"})
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
